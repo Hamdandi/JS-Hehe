f3e0fcd19e250702f7af281ac8a2979085ac990a
Hamdandi <90332110+Hamdandi@users.noreply.github.com>
2021-10-09 20:16:01 +0700

    Add files via upload

---
+++ Belajar JS/ELSE_IF.js
@@ -0,0 +1,10 @@
+var angka = prompt ('masukkan angka :');
+
+if (angka % 2 == 1){
+    alert(angka + ' angka yang anda masukkan merupakan bilangan GANJIL')
+}else if ( angka % 2 == 0){
+    alert(angka + ' angka yang anda masukkan merupakan bilangan GENAP')
+}else {
+    alert('tolong masukkan ANGKA jangan HURUF THOLOL')
+}
+ 
---
+++ Belajar JS/IF_ELSE.js
@@ -0,0 +1,7 @@
+var angka = prompt ('masukkan angka :');
+
+if (angka % 2 == 1){
+    alert(angka + ' angka yang anda masukkan merupakan bilangan GANJIL')
+}else {
+    alert(angka + ' angka yang anda masukkan merupakan bilangan GENAP')
+}
